5705629b7c8f11d971bd49ef0476df8300622232
jabinzou <jabinzou1@gmail.com>
2019-07-12 20:46:25 +0800

    [performance]修改文件存储，写文件并行

--- proxy.py
+++ proxy.py
@@ -1,19 +1,28 @@
+
+# -*- coding: utf-8 -*-
+
+import sys
 from bs4 import BeautifulSoup
-# import queue
 import requests
+reload(sys)
+sys.setdefaultencoding('utf-8')
 url='http://www.xicidaili.com/nn/'
 headers={
-      'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',        
+  'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',        
 }
 ip_list=[]
-for a in range(0, 10):
+for a in range(0, 20):
   ip_data=requests.get(url+bytes(a), headers=headers)
   soup=BeautifulSoup(ip_data.text,'html.parser')
   ips=soup.select('tr')
   for i in range(1,len(ips)):
     ip_info=ips[i]
     tds=ip_info.select('td')
-    ip_list.append(tds[1].text+':'+tds[2].text)
+    quality = ip_info.select('.country .bar div')
+    # 过滤高可用的ip
+    if (int((quality[0]['style'].split(':')[1]).encode("utf-8").replace('%', '')) >= 90
+      and int((quality[1]['style'].split(':')[1]).encode("utf-8").replace('%', '')) >= 90):
+      ip_list.append(tds[1].text+':'+tds[2].text)
 f = open("iplist.txt",'w')
 for ip in ip_list:
     f.write(ip)
--- request.py
+++ request.py
@@ -4,14 +4,13 @@ import sys
 import requests
 import linecache
 from bs4 import BeautifulSoup
-from xlwt import *
-import xlwt
+from openpyxl import Workbook,load_workbook
 from tqdm import tqdm
 import time
 import random
 from multiprocessing import Process,Pool,Manager
+from xlutils.copy import copy
 import os
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 # 获取文件制定行数，是第一次读取后面直接从缓存中拿
@@ -30,33 +29,37 @@ def request_line(startInfo = ['','','', ''], endInfo = ['','','', ''], composeDa
     '_fm.es._0.rec': endInfo[2]
   }
   try:
-    list = get_ip_list()
-    proxy = get_random_ip(list)
+    proxy = get_random_ip(proxy_list)
     r = requests.get('https://56.1688.com/order/price/estimate_price.htm?notFirst=true&_fm.es._0.o=&_fm.es._0.c=&_fm.es._0.co=&_fm.es._0.sp=&_fm.es._0.i=&_fm.es._0.is=&_fm.es._0.isf=&_fm.es._0.isp=&_fm.es._0.iscn=&_fm.es._0.isn=&_fm.es._0.isc=&_fm.es._0.isa=&_fm.es._0.ro=&_fm.es._0.w=&_fm.es._0.we=&_fm.es._0.v=&_fm.es._0.vo=&_fm.es._0.tra=&_fm.es._0.tran=&_fm.es._0.isfr=&_fm.es._0.a=&_fm.es._0.d=&_fm.es._0.d=&_fm.es._0.d=&_fm.es._0.d=&r=1560672972573&sizePerPage=&pageIndex=1&weight=&expressRouteSortType=&_fm.es._0.sentc=',proxies=proxy ,params=param)
     soup = BeautifulSoup(r.text, 'lxml')
     tags = soup.select('#orderbyBar span[style="font-weight:bold;color:#FF7300"]')
-    composeData.append([startInfo[3].replace('\n', ''), endInfo[3].replace('\n', ''),tags[0].get_text(), tags[1].get_text()])
+    composeData.append([startInfo[3].replace('\n', '').decode('utf-8'), endInfo[3].replace('\n', '').decode('utf-8'),tags[0].get_text().decode('utf-8'), tags[1].get_text().decode('utf-8')])
   except Exception as e:
     # print(e, r.text)
     print('\n'+startInfo[3]+'-'+endInfo[3]+'\n')
+  finally:
+    return composeData
 
+# 读取ip文件
 def get_ip_list():
-    temp_ip_list=[]
-    with open('iplist.txt', 'r') as f:
-      while True:
-        ip=f.readline().replace('\n','')
-        temp_ip_list.append('http://'+ip)
-        if not ip:
-            break
-    return temp_ip_list
-     
-def get_random_ip(list):        
-    proxy_ip=random.choice(list)
-    proxies={
-        'http':proxy_ip
-    }
-    return proxies
+  temp_ip_list=[]
+  with open('iplist.txt', 'r') as f:
+    while True:
+      ip=f.readline().replace('\n','')
+      temp_ip_list.append('http://'+ip)
+      if not ip:
+          break
+  return temp_ip_list
 
+# 获取随机代理ip
+def get_random_ip(list):      
+  proxy_ip=random.choice(list)
+  proxies={
+      'http':proxy_ip
+  }
+  return proxies
+
+# 组合对应的请求数据参数
 def pollData():
   queryInfo = []
   for line in range(start, end+1):
@@ -66,45 +69,48 @@ def pollData():
       queryInfo.append([[resData[0], resData[2],'', resData[1]+resData[3]], [toData[0], toData[2],'', toData[1]+toData[3]]])
   return queryInfo
 
+# 初始化对应的数据
 def init():
   global start
   global end
   global pbar
+  global sheet_name
+  global slice_num # 数组切割次数
+  global proxy_list
+  slice_num = 300
+  sheet_name = '0712.xlsx'
+  wb = Workbook()
+  wb.active.append(['起始位置','终点位置', '公司数量', '线路数量'])
+  wb.save(sheet_name)
   start = input('请输入开始行数:')
   end = input('请输入结束行数:')
+  proxy_list = get_ip_list()
   pbar = tqdm(total=((end-start+1) * (end-start+1)))
 
-def saveFile():
-  file = Workbook(encoding = 'utf-8')
-  table = file.add_sheet('广东省物流数据')
-  table.col(0).width = 256 * 24
-  table.col(1).width = 256 * 24
-  font = xlwt.Font()  # Create Font
-  style = xlwt.XFStyle()  # Create Style
-  font.height = 20 * 14  # 字体大小
-  style.font = font
-  try: 
-    for i,p in enumerate(composeData):
-      for j,q in enumerate(p):
-        # print i,j,q
-        table.write(i,j,q,style)
-    file.save('nation.xlsx')
-  except Exception as e:
-    f = open("nation.txt",'w')
-    for da in composeData:
-      print da
-      f.write(','.join(da))
-      f.write('\n')
-
+#更新进度条 并且进行追加数据到excel中
 def updatePb(res):
   pbar.update(1)
+  if (len(res) >= slice_num):
+    saveFile_append(res)
+# 追加写入excel
+def saveFile_append(res):
+  workbook = load_workbook(sheet_name)  # 打开工作簿 保留原有格式
+  sheets = workbook.active  # 获取工作簿中的所有表格
+  for i in res[0:slice_num+1]:
+    sheets.append(i)
+  workbook.save(sheet_name)  # 保存工作簿
+  removeList(res)
 
+# 移除数组中的已经写入的数据
+def removeList(composeData):
+  for i in composeData[0:(slice_num+1)]:
+    composeData.remove(i)
 def main():
   init()
   global composeData
   compose = pollData()
   manager = Manager()
-  composeData = manager.list([['起始位置','终点位置', '公司数量', '线路数量']])
+  composeData = manager.list([]) # 进程数据共享
   pool = Pool(4)
   print '\nParent process %s'%os.getpid()
   for i in compose:
@@ -112,8 +118,8 @@ def main():
   pool.close()
   pool.join()
   pbar.close()
+  saveFile_append(composeData) # 最后将剩余的数据写入excel中
   print '\ndone'
-  saveFile()
 
 if __name__=='__main__':
   main()
